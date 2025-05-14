9f5bdd6a6f41d70601f575d29fb1f250b7aaeba3
JoseVinicius-P <josevpf123@gmail.com>
2025-05-14 16:04:38 -0300

    fix: Corrigindo endereço da api

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "309376685"
+    serviceWorkerVersion: "3166443964"
   }
 });
