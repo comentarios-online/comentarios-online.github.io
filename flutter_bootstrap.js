99dde636f07e2c249fb5362d0d846ebbf7b28801
JoseVinicius-P <josevpf123@gmail.com>
2025-05-18 17:08:18 -0300

    refactor: testando webserver

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "471563447"
+    serviceWorkerVersion: "1563287680"
   }
 });
