bc6fe5999dc47228ca9983ddd9e65664bf4c2658
JoseVinicius-P <josevpf123@gmail.com>
2025-05-21 16:31:37 -0300

    fix: Corrigindo bug que impedia recarregamento da página em alguns casos

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2711981962"
+    serviceWorkerVersion: "2004495868"
   }
 });
